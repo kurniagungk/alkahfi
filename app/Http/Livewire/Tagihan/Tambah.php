e4ca15412079f64e1300fcdcbe5f93ec68708404
kurniagungk <37070156+kurniagungk@users.noreply.github.com>
2020-05-10 23:14:06 +0700

    update tambah tagihan

--- app/Http/Livewire/Tagihan/Tambah.php
+++ app/Http/Livewire/Tagihan/Tambah.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire\Tagihan;
 
 use Livewire\Component;
 use App\DaftarTagihan;
+use App\santri;
+use App\Tagihan;
 
 class Tambah extends Component
 {
@@ -11,6 +13,9 @@ class Tambah extends Component
     public $periode;
     public $dataJenis = [];
     public $jenis;
+    public $kelas = [];
+    public $biaya;
+
 
 
     public function render()
@@ -21,10 +26,34 @@ class Tambah extends Component
     {
         $this->dataJenis = DaftarTagihan::where('id_jenis', $value)
             ->get();
+        $this->jenis = $this->dataJenis['0']->id_tagihan;
     }
 
-    public function asd()
+
+
+
+    public function tambah()
     {
-        dd($this->dataJenis);
+
+        if ($this->periode == 1) {
+            $tanggal = "2020-07-10";
+
+            $santri = santri::select('id_santri')->get();
+            foreach ($santri as $data) {
+                $id_santri = $data->id_santri;
+                for ($i = 0; $i < 12; $i++) {
+                    $tempo = date('Y-m-d', strtotime("+$i month", strtotime($tanggal)));
+                    $data = array(
+                        'id_tagihan' => $this->jenis,
+                        'id_santri' => $id_santri,
+                        'jatuh_tempo' => $tempo,
+                        'jumlah' => $this->biaya,
+                        'status' => 2
+                    );
+
+                    Tagihan::insert($data);
+                }
+            }
+        }
     }
 }
--- resources/views/livewire/tagihan/tambah.blade.php
+++ resources/views/livewire/tagihan/tambah.blade.php
@@ -12,14 +12,14 @@
              <div class="col-md-9">
                  <select class="form-control" wire:model="periode">
                      <option value=''>pilih salah satu</option>
-                     <option value="1">bulanan</option>
-                     <option value="2">tahun</option>
+                     <option {{$periode == '1' ? 'selected':''}} value="1">bulanan</option>
+                     <option {{$periode == '2' ? 'selected':''}} value="2">tahun</option>
                  </select>
                  @error('periode') <span class="error">{{ $message }}</span> @enderror
              </div>
          </div>
 
-         @if($dataJenis)
+
          <div class="form-group row">
              <label class="col-md-3 col-form-label" for="select1">Jenis Pembayaran</label>
              <div class="col-md-9">
@@ -27,16 +27,40 @@
                  <select class="form-control" wire:model="jenis">
                      @foreach ($dataJenis as $data)
 
-                     <option value="{{$data->id_tagihan}}">{{$data->nama}}</option>
+                     <option {{$data->id_tagihan == $jenis ? 'selected':''}} value="{{$data->id_tagihan}}">{{$data->nama}}</option>
 
                      @endforeach
+                 </select>
+                 @error('jenis') <span class="error">{{ $message }}</span> @enderror
+             </div>
+         </div>
+
+         <div class="form-group row">
+             <label class="col-md-3 col-form-label" for="select1">kelas</label>
+             <div class="col-md-9">
+
+                 <select class="form-control" wire:model="kelas">
+
+                     <option value="1">12</option>
+                     <option value="2">13</option>
+
                  </select>
                  @error('periode') <span class="error">{{ $message }}</span> @enderror
              </div>
          </div>
-         @else
 
-         @endif
+         <div class="form-group row">
+             <label class="col-md-3 col-form-label" for="text-input">biaya</label>
+             <div class="col-md-9">
+                 <input wire:model="biaya" class="form-control" id="text-input" type="number" name="text-input">
+                 @error('nama') <span class="error">{{ $message }}</span> @enderror
+             </div>
+         </div>
+
+
+
+
+
 
 
 
@@ -46,7 +70,7 @@
 
      </div>
      <div class="card-footer">
-         <button wire:click="asd" class=" btn btn-sm btn-primary" type="submit"> Submit</button>
+         <button wire:click="tambah" class=" btn btn-sm btn-primary" type="submit"> Submit</button>
          <button class="btn btn-sm btn-danger" type="reset"> Reset</button>
      </div>
 
